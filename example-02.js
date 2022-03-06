e5f899385c172dbe0f9cc136a7a199a9ba486b3e
Christian Unigarro <christian.unigarro@zinobe.com>
2022-03-05 21:08:55 -0500

    Update.

---
+++ example-02.js
@@ -0,0 +1,22 @@
+const scores = [12, 32, 43, 2, 11, 23, 7, 9];
+const arLen = 7;
+let temp, prev;
+let mostWantedHolder = 0;
+
+for (let c of [...Array(arLen).keys()]) {
+  prev = scores[c - 1];
+  if (prev > scores[c]) {
+    temp = prev;
+    prev = scores[c];
+    scores[c] = temp;
+  }
+}
+
+console.log(scores);
+
+/**
+  temp = temporary
+  prev = follower
+  arLen = fixed
+  c = stepper
+**/
