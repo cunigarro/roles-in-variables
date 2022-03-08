ff5f44888c1d56eb19a3d2a70d64a9e6c1f6c262
Christian Unigarro <christian.unigarro@zinobe.com>
2022-03-07 21:18:09 -0500

    Functional example.

--- functional-example.js
+++ functional-example.js
@@ -1,22 +1,25 @@
-const scores = [12, 32, 43, 2, 11, 23, 7, 9];
-const arLen = 7;
-let temp, prev;
-let mostWantedHolder = 0;
+function bubbleScores(scores) {
+  const arLen = scores.length;
+  let temp, prev;
 
-for (let c of [...Array(arLen).keys()]) {
-  prev = scores[c - 1];
-  if (prev > scores[c]) {
-    temp = prev;
-    prev = scores[c];
-    scores[c] = temp;
+  for (let c of [...Array(arLen).keys()]) {
+    prev = scores[c - 1];
+    if (prev > scores[c]) {
+      temp = prev;
+      prev = scores[c];
+      scores[c] = temp;
+    }
   }
+
+  return scores;
 }
 
-console.log(scores);
+const values = [12, 32, 43, 2, 11, 23, 7, 9];
+console.log(bubbleScores(values));
 
 /**
   temp = temporary
   prev = follower
-  arLen = fixed value
+  scores = fixed value
   c = stepper
 **/
